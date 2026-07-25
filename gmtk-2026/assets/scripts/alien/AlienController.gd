b0c557d0c36fc48346443208fa93136960ad2407
BiscuitPrime <henri.nomico@gmail.com>
2026-07-25 23:46:17 +0200

    Added working aliens fuk aff ya twisty bangeridoo

--- gmtk-2026/assets/scripts/alien/AlienController.gd
+++ gmtk-2026/assets/scripts/alien/AlienController.gd
@@ -1,5 +1,9 @@
-class_name AlienController extends TextureRect
+class_name AlienController extends Node2D
 # Controls the alien
+
+@export var Aliens : Dictionary[int, AnimatedSprite2D];
+var _currentAlien : AnimatedSprite2D;
+var _randomNumberGenerator = RandomNumberGenerator.new();
 
 enum ALIEN_MOOD{
 	IDLE,
@@ -7,12 +11,29 @@ enum ALIEN_MOOD{
 	DEFEAT	
 };
 
+func _ready()->void:
+	var rng = _randomNumberGenerator.randi_range(0, Aliens.size()-1);
+	SignalBus.on_win.connect(ChangeAlienAnimation.bind(ALIEN_MOOD.DEFEAT));
+	SignalBus.on_loss.connect(ChangeAlienAnimation.bind(ALIEN_MOOD.VICTORY));
+	DeactivateAllAliens();
+	ActivateAlien(Aliens.keys()[rng]);
+
+func DeactivateAllAliens()->void:
+	for alien in Aliens:
+		Aliens[alien].visible = false;
+	return;
+
+func ActivateAlien(alienKey:int)->void:
+	Aliens[alienKey].visible=true;
+	_currentAlien=Aliens[alienKey];
+	ChangeAlienAnimation(ALIEN_MOOD.IDLE);
+	return;
+
 func ChangeAlienAnimation(mood:ALIEN_MOOD)->void:
 	match(mood):
 		ALIEN_MOOD.IDLE:
-			print("idle");
+			_currentAlien.play("idle");
 		ALIEN_MOOD.VICTORY:
-			print("vic");
+			_currentAlien.play("victory");
 		ALIEN_MOOD.DEFEAT:
-			print("defeat");
-		
+			_currentAlien.play("defeat");
