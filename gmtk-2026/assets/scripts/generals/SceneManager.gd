771eaa60439504c5ef4b4ac653b00857d61a5503
Hugo Carbiener <41008899+Hugo-Carbiener@users.noreply.github.com>
2026-07-26 16:47:17 +0200

    Add titlescreen

--- gmtk-2026/assets/scripts/generals/SceneManager.gd
+++ gmtk-2026/assets/scripts/generals/SceneManager.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
+var titlescreen : PackedScene = preload("res://assets/scenes/title-screen.tscn");
 var encounter : PackedScene = preload("res://assets/scenes/level.tscn");
 var shop : PackedScene = preload("res://assets/scenes/shop.tscn");
 
 func _ready() -> void:
 	SignalBus.shop_clicked.connect(open_shop);
 	SignalBus.next_encounter_clicked.connect(start_encounter);
+
+func menu():
+	get_tree().change_scene_to_packed(titlescreen);
 
 func open_shop():
 	get_tree().change_scene_to_packed(shop);
